06f143cb5aac55de861456a732ac480b8f19b4a4
Steven Liu <steeven.liu2@gmail.com>
2019-07-22 15:12:43 +1000

    window management works

--- treemodule.js
+++ treemodule.js
@@ -1,3 +1,7 @@
-chatTreeCore.registerModule("tree",function(window){
+chatTreeCore.registerModule("tree", function (core,window) {
+    let p = document.createElement("p");
+    p.innerHTML="Hello world window!"
+    window.appendChild(p);
+    
 
 })
